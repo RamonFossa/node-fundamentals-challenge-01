b7520621dc74aaa453cd7537e92de667d1418460
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-03-03 14:13:03 -0300

    refact: Create task only if all required data is in the request

--- src/controllers/createTask.js
+++ src/controllers/createTask.js
@@ -1,8 +1,11 @@
 import { database } from "../database.js";
 
 export function createTask(req, res) {
-    try {
-        const { title, description } = req.body;
+        const title = req.body.title;
+        const description = req.body.description;
+
+        if(!title) return res.writeHead(406).end('Title is required to create a Task!');
+        if(!description) return res.writeHead(406).end('Description is required to create a Task!');
 
         const task = {
             title,
@@ -13,9 +16,4 @@ export function createTask(req, res) {
 
         const tasks = database.insert('tasks', task);
         return res.writeHead(201).end(JSON.stringify(tasks));
-
-    } catch (error) {
-        console.error(error);
-        return res.writeHead(406).end('Title and Description are required to create a Task!');
-    }
 }
