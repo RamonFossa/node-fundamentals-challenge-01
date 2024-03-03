749c9c450362e99abd2dccc7a11f3d7631c6081a
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-03-03 14:31:11 -0300

    refact: Change updateTask route to update only not completed tasks

--- src/controllers/updateTask.js
+++ src/controllers/updateTask.js
@@ -7,6 +7,14 @@ export function updateTask(req, res) {
 
     if(description === undefined && title === undefined) return res.writeHead(406).end('Title or description is required to update a task!');
     
+    const databaseRes = database.select('tasks', id);
+
+    const { error, message } = databaseRes;
+
+    if (error) return res.writeHead(404).end(message);
+
+    if(message.completed_at !== null) return res.writeHead(403).end('Can`t update a completed task!')
+
     const updated_at = Date.now();
 
     const updateBody = {updated_at};
@@ -14,9 +22,7 @@ export function updateTask(req, res) {
     if(description) updateBody['description'] = description;
     if(title) updateBody['title'] = title;
     
-    const databaseRes = database.update('tasks', id, updateBody);
+    const dbResponse = database.update('tasks', id, updateBody).message;
 
-    const { error, message } = databaseRes;
-
-    return res.writeHead(error ? 404 : 200).end(message);
+    return res.writeHead(200).end(dbResponse);
 }
